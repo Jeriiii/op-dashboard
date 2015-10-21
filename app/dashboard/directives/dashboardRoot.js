7ba9087044ed42477b75e0acd3b4999c62f51b87
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-10-21 10:56:06 +0200

    Převedení logiky vakreslení widgetů do šablony

--- app/dashboard/directives/dashboardRoot.js
+++ app/dashboard/directives/dashboardRoot.js
@@ -1,67 +1,50 @@
+/* funkce controleru této direktivy */
+var ctrlFnc = function ( $scope, $element ) {
+  $scope.addTextWidget = function () {
+    $scope.widgets.push({
+        type: 'simpleText',
+        settings: {
+          tittle: 'Vygenerovaný textový widget',
+          text: 'Toto je opravdu jednoduchý dashboard 1 jen s textem'
+        }
+      });
+  };
+
+  $scope.addGraphWidget = function () {
+    $scope.widgets.push({
+      type: 'graph',
+      settings: {
+        tittle: 'Vygenerované graf',
+        relativeUrl: 'data/graph1.json'
+      }
+    });
+  };
+
+  $scope.addClockWidget = function () {
+    $scope.widgets.push({
+      type: 'clock',
+      settings: {
+        tittle: 'Vygenerované hodiny',
+        format: 'HH:mm:ss'
+      }
+    });
+  };
+};
+
+/* funkce link této direktivy */
+var linkFnc = function(scope, elem, attrs) {
+  var model = JSON.parse(attrs.dbModel);
+  //var model = dashboardModelProvider.getModel();
+  scope.widgets = model.widgets;
+};
+
 // Hlavní direktiva, ve které se nachází celý dashboard
 dashboardApp.directive('dashboardRoot', ['$compile', 'dashboardModel', function($compile, dashboardModelProvider) {
   return {
     restrict: 'E',
     replace: true,
     templateUrl: 'dashboard/templates/rootDashboart.html',
-
-    controller: function ( $scope, $element ) {
-
-      var compileWidget =   function (settings, htmlToCompile) {
-        $scope.settings = settings;
-        var el = $compile(htmlToCompile)( $scope );
-        $element.find('#content').append( el );
-      };
-
-      $scope.addTextWidget = function (settings) {
-        var htmlToCompile = '<widget-base dbw-title="' + settings.tittle + '"><widget-simple-text dbw-text="' + settings.text + '" /></widget-base>';
-
-        compileWidget(settings, htmlToCompile);
-      };
-
-      $scope.addGraphWidget = function (settings) {
-        var htmlToCompile = '<widget-base dbw-title="' + settings.tittle + '"><graph-example relative-url="' + settings.relativeUrl + '" /></widget-base>';
-
-        compileWidget(settings, htmlToCompile);
-      };
-
-      $scope.addClockWidget = function (settings) {
-        var htmlToCompile = '<widget-base dbw-title="' + settings.tittle + '"><widget-clock format="' + settings.format + '" /></widget-base>';
-
-        compileWidget(settings, htmlToCompile);
-      };
-
-    },
-
-    link: function(scope, elem, attrs) {
-      var model = JSON.parse(attrs.dbModel);
-      //var model = dashboardModelProvider.getModel();
-
-      angular.forEach(model.widgets, function(value, key) {
-        if(value.type == 'simpleText') {
-          scope.addTextWidget(value.settings);
-        } else if(value.type == 'graph') {
-          scope.addGraphWidget(value.settings);
-        } else if(value.type == 'clock') {
-          scope.addClockWidget(value.settings);
-        }
-      });
-
-      scope.textSettings = {
-        tittle: 'Vygenerovaný textový widget',
-        text: 'Obsah vygenerovaného widgetu.'
-      };
-
-      scope.graphSettings = {
-        tittle: 'Vygenerovaný textový widget',
-        relativeUrl: 'data/graph1.json'
-      };
-
-      scope.clockSettings = {
-        tittle: 'Vygenerované hodiny',
-        format: 'HH:mm:ss'
-      };
-    }
-
+    controller: ctrlFnc,
+    link: linkFnc
   };
 }]);
