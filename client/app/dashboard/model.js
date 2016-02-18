50d46e31359a0fc17450f003fcf78a711243f186
Petr Kukrál <p.kukral@jkbusiness.cz>
2016-02-18 13:08:06 +0100

    rozsekání modelu na více menších widgetů, které je možné přidávat samostatně

--- client/app/dashboard/model.js
+++ client/app/dashboard/model.js
@@ -1,77 +1,89 @@
-var model = {
-  widgets: [
-    {
-      type: 'csschart',
-      settings: {
-        tittle: 'Csschart',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
-      }
-    },
-    {
-      type: 'pieNg',
-      settings: {
-        tittle: 'Pie chart Ng',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=pie'
-      }
-    },
-    {
-      type: 'barchartJQ',
-      settings: {
-        tittle: 'Bar chart JQ',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
-      }
-    },
-    {
-      type: 'barchartNg',
-      settings: {
-        tittle: 'Bar chart ng',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
-      }
-    },
-    {
-      type: 'barchartReact',
-      settings: {
-        tittle: 'Bar chart React',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
-      }
-    },
-    {
-      type: 'linechart',
-      settings: {
-        tittle: 'Linechart',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=linechart'
-      }
-    },
-    {
-      type: 'linechart-ang',
-      settings: {
-        tittle: 'Linechart Angular',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=linechart'
-      }
-    },
-    {
-      type: 'simpleText',
-      settings: {
-        tittle: 'Jednoduchý widget 1',
-        text: 'Toto je opravdu jednoduchý dashboard 1 jen s textem <b>ahoj</b>',
-        escapeHtml: false
-      }
-    },
-    {
-      type: 'clock',
-      settings: {
-        tittle: 'Hodiny',
-        format: 'HH:mm:ss'
-      }
-    },
-    {
-      type: 'graph',
-      settings: {
-        tittle: 'Jednoduchý graf 2',
-        relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=hightchart'
-      }
+/* direktivy - klasické */
+var simpleText = {
+  type: 'simpleText',
+  settings: {
+    tittle: 'Jednoduchý widget 1',
+      text: 'Toto je opravdu jednoduchý dashboard 1 jen s textem <b>ahoj</b>',
+      escapeHtml: false
+  }
+};
+
+var clock = {
+  type: 'clock',
+  settings: {
+    tittle: 'Hodiny',
+    format: 'HH:mm:ss'
+  }
+};
+
+var graph = {
+  type: 'graph',
+  settings: {
+    tittle: 'Jednoduchý graf 2',
+    relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=hightchart'
+  }
+};
+
+/* direktivy - grafy */
+var csschart = {
+  type: 'csschart',
+  settings: {
+    tittle: 'Csschart',
+    relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
+  }
+};
+
+var pieNg = {
+  type: 'pieNg',
+  settings: {
+    tittle: 'Pie chart Ng',
+    relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=pie'
+  }
+};
+
+var barchartJQ = {
+  type: 'barchartJQ',
+  settings: {
+    tittle: 'Bar chart JQ',
+    relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
+  }
+};
+
+var barchartNg = {
+  type: 'barchartNg',
+  settings: {
+    tittle: 'Bar chart ng',
+    relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
     }
-  ]
+};
+
+var barchartReact = {
+  type: 'barchartReact',
+  settings: {
+    tittle: 'Bar chart React',
+    relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=barchart'
+    }
+};
+
+var linechart = {
+  type: 'linechart',
+  settings: {
+    tittle: 'Linechart',
+    relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=linechart'
+    }
+};
+
+var linechartNg = {
+type: 'linechart-ang',
+settings: {
+  tittle: 'Linechart Angular',
+  relativeUrl: 'http://localhost/skola/op/dashboard-op/server/?type=linechart'
+  }
+};
+
+var model = {
+  widgets: [csschart, pieNg, barchartJQ, barchartNg, barchartReact, linechart, linechartNg,
+    simpleText, clock, graph]
 };
 
 dashboardApp.value('dModel', model);
