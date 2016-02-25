a8d04bae65066210d10884f736096f76c4b712b6
Petr Kukrál <p.kukral@jkbusiness.cz>
2016-02-25 13:54:33 +0100

    Refaktoring

--- client/app/dashboard/widgets/baseWidget/directive.js
+++ client/app/dashboard/widgets/baseWidget/directive.js
@@ -1,4 +1,4 @@
-// Prázdný widget
+// Obalovací widget
 dashboardApp.directive('widgetBase', function() {
   return {
     restrict: 'E',
@@ -8,7 +8,7 @@ dashboardApp.directive('widgetBase', function() {
     link: function(scope, elem, attrs) {
         scope.dbwTitle = attrs.dbwTitle;
 
-        /* funkce co smaže widget. V této fci se ještě dá udělat ošetření smazání, či vyhodit modal okno */
+        /* Funkce co smaže widget.*/
         var deleteWidget = function() {
           elem.remove();
           scope.$broadcast('$destroy');
